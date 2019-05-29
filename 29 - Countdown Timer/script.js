26b45a61fed1c6955bc696fb443ca8203f17af72
Guilherme Garber <ggarber@protonmail.com>
2019-05-29 12:51:52 -0300

    29 pronto

--- 29 - Countdown Timer/script.js
+++ 29 - Countdown Timer/script.js
@@ -1,14 +1,14 @@
-let countdonw;
+let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds){
-    clearInterval(countdonw);
+    clearInterval(countdown);
     const now = Date.now();
     const then = now + seconds*1000;
     displayTimeLeft(seconds);
-    displayEndTime(then)
+    displayEndTime(then);
     countdown = setInterval(()=>{
         const secondsLeft = Math.round((then - Date.now())/1000);
         if(secondsLeft <= 0){
@@ -42,3 +42,9 @@ function startTimer(){
 }
 
 buttons.forEach(button => button.addEventListener('click',startTimer))
+document.customForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins*60);
+    this.reset();
+});
